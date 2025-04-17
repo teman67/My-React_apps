bf64fdb4a3604f565765bd54b3727953adb28698
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2025-04-17 17:17:53 +0200

    finish the converter app

--- currency_converter/src/App.js
+++ currency_converter/src/App.js
@@ -1,22 +1,65 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
+import { useState, useEffect, use } from "react";
+
 export default function App() {
+  const [amount, setAmount] = useState(1);
+  const [from, setFrom] = useState("EUR");
+  const [to, setTo] = useState("USD");
+  const [result, setResult] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true);
+      const response = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`
+      );
+      const data = await response.json();
+      console.log(data.rates[to]);
+      setResult(data.rates[to]);
+      setIsLoading(false);
+    }
+
+    if (from === to) {
+      setResult(amount);
+      return;
+    }
+    fetchData();
+  }, [amount, from, to]);
+
   return (
     <div>
-      <input type="text" />
-      <select>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={isLoading}
+      />
+
+      <select
+        value={from}
+        onChange={(e) => setFrom(e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select
+        value={to}
+        onChange={(e) => setTo(e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>
+        {result} {to}{" "}
+      </p>
     </div>
   );
 }
